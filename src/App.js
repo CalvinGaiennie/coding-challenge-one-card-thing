649af6147db08d110ca9c3623b7d7e7248d882ff
CalvinGaiennie <calvingaiennie@gmail.com>
2024-11-22 13:44:58 -0600

    started challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar src="/download.jpeg" name="Jonas Schmedtman" />
+      <div className="data">
+        <Intro text={"Fledgling Developer blah blah blah"} />
+        <Skill text="Web Dev" />
+        <SkillList />
+      </div>
     </div>
   );
 }
+
+function Avatar(props) {
+  return (
+    <div>
+      <img src={props.src} alt="avatar"></img>
+      <h1>{props.name}</h1>
+    </div>
+  );
+}
+function Intro(props) {
+  return <p>{props.text}</p>;
+}
+
+function Skill(props) {
+  return <div className="skill">{props.text}</div>;
+}
+
+function SkillList() {}
 
 export default App;
